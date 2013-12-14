d725993054603fd75445e77fe827527ed9ab2cc5
grant-humphries <grant.humphries@gmail.com>
2013-12-13 18:43:51 -0800

    added network analyst instructions

--- network_analyst/foot_permissions.py
+++ network_analyst/foot_permissions.py
@@ -1,6 +1,3 @@
-# Settings for this network attribute:
-# Usage Type: Restriction
-# Restriction Usage: Prohibited
 def footPermissions(foot, access, highway):
 	if foot in ('yes', 'designated', 'permissive'):
 		return False
@@ -11,9 +8,6 @@ def footPermissions(foot, access, highway):
 
 footPermissions(!foot!, !access!, !highway!)
 
-# Usage Type: Cost
-# Units: leave as 'Unknown', see below for details
-# Data Type: Double
 # length is assume to be in feet and walk speed is 3 miles per hour in this case
 def walkMinutes(length):
 	walk_time = length / (5280 * (3 / float(60)))
@@ -25,6 +19,5 @@ walkMinutes(!Shape!)
 # NOTE!!!! Building the network dataset may not work if you set the units for the walk length attribute
 # to minutes, or if you set it at all, this seems to be a bug
 
-# Also the PostGIS streets and trails layer is being saved as a shape file by QGIS is currently adding 
-# leading spaces in front of the attributes values, these must be removed for the functions above to work
-# (use python 'field'.strip() in field calculator)
+# Also the output from PostGIS, QGIS is currently adding leading spaces in front of the attributes
+# values, these must be removed for the functions above to work (use python 'field'.strip() in field calculator)
