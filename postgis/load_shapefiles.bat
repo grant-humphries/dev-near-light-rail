8cf513f0e05d5ec4ccb6126067b90e1a59fef40c
grant-humphries <grant.humphries@gmail.com>
2014-04-28 12:19:20 -0700

    rewrote load_shapefiles as a batch file (instead of bash)

--- postgis/load_shapefiles.bat
+++ postgis/load_shapefiles.bat
@@ -1,35 +1,49 @@
-# First set the password for the postgres database so that it doesn't have to be repeatedly entered
-#set pgpassword=**********
-# commenting this out for now so my password isn't on github
+::Set postgres parameters
+set pg_host=localhost
+set db_name=transit_dev
+set pg_user=postgres
 
-# PROJECT DATA (these were created in earlier phases of the project, most are modified 
-# versions of RLIS or TriMet data).  
-
-# **NOTE:** The $1 sub-folder in the file path of 'project data' datasets must be replaced with the name of the folder that has been created for the current iteration of the project.  Don't push this change to github as it could lead to inadvertant use old files future iterations of the project
-
-# MAX Stops 
-shp2pgsql -s 2913 -d -I //gisstore/gis/PUBLIC/GIS_Projects/Development_Around_Lightrail/data/$1/max_stops.shp max_stops | psql -U postgres -d transit_dev
-
-# Walkshed Polygons (Isocrones)
-shp2pgsql -s 2913 -d -I //gisstore/gis/PUBLIC/GIS_Projects/Development_Around_Lightrail/data/$1/max_stop_isocrones.shp isocrones | psql -U postgres -d transit_dev
-
-# Trimmed Taxlots
-shp2pgsql -s 2913 -d -I //gisstore/gis/PUBLIC/GIS_Projects/Development_Around_Lightrail/data/$1/trimmed_taxlots.shp taxlot | psql -U postgres -d transit_dev
-
-# Trimmed Multi-family Housing
-shp2pgsql -s 2913 -d -I //gisstore/gis/PUBLIC/GIS_Projects/Development_Around_Lightrail/data/$1/trimmed_multifam.shp multi_family | psql -U postgres -d transit_dev
+::Prompt the user to enter their postgres password, 'pgpassword' is a keyword and will automatically
+::set the password for most postgres commands in the current session
+set /p pgpassword="Enter postgres password:"
 
 
-# TRIMET DATA
+::PROJECT DATA (these were created in earlier phases of the project, most are modified 
+::versions of RLIS or TriMet data).  
+::Set input parameters
+set srid=2913
 
-# TriMet Service District Boundary
-shp2pgsql -s 2913 -d -I //gisstore/gis/TRIMET/tm_fill.shp tm_district | psql -U postgres -d transit_dev
+::Prompt the user to enter the name of the sub-folder holding current project datasets
+set /p proj_folder="Enter the name of the sub-folder holding the data for this interation of the project (should be in the form 'YYYY_MM'): "
+set proj_path=G:/PUBLIC/GIS_Projects/Development_Around_Lightrail/data/%proj_folder%
+
+::MAX Stops 
+shp2pgsql -s %srid% -d -I %proj_path%/max_stops.shp max_stops | psql -h %pg_host% -U %pg_user% -d %db_name%
+
+::Walkshed Polygons (Isocrones)
+shp2pgsql -s %srid% -d -I %proj_path%/max_stop_isocrones.shp isocrones | psql -h %pg_host% -U %pg_user% -d %db_name%
+
+::Trimmed Taxlots
+shp2pgsql -s %srid% -d -I %proj_path%/trimmed_taxlots.shp taxlot | psql -h %pg_host% -U pg_user% -d %db_name%
+
+::Trimmed Multi-family Housing
+shp2pgsql -s %srid% -d -I %proj_path%/trimmed_multifam.shp multi_family | psql -h %pg_host% -U pg_user% -d %db_name%
 
 
-# RLIS DATA
+::TRIMET DATA
+::Set path to data folder
+set trimet_path=G:/TRIMET
 
-# City Boundaries
-shp2pgsql -s 2913 -d -I //gisstore/gis/Rlis/BOUNDARY/cty_fill.shp city | psql -U postgres -d transit_dev
+::TriMet Service District Boundary
+shp2pgsql -s %srid% -d -I %trimet_path%/tm_fill.shp tm_district | psql -h %pg_host% -U pg_user% -d %db_name%
 
-# Urban Growth Boundary
-shp2pgsql -s 2913 -d -I //gisstore/gis/Rlis/BOUNDARY/ugb.shp ugb | psql -U postgres -d transit_dev
+
+::RLIS DATA
+::Set path to data folder
+set rlis_path=G:/Rlis
+
+::City Boundaries
+shp2pgsql -s %srid% -d -I %rlis_path%/BOUNDARY/cty_fill.shp city | psql -h %pg_host% -U pg_user% -d %db_name%
+
+::Urban Growth Boundary
+shp2pgsql -s %srid% -d -I %rlis_path%/BOUNDARY/ugb.shp ugb | psql -h %pg_host% -U pg_user% -d %db_name%
