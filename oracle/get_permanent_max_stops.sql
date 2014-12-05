7e93619b3a84433f5bca6a40f4ed40dc82af1190
grant-humphries <grant.humphries@gmail.com>
2014-12-05 15:42:09 -0800

    added a couple of missing conditions to the permanent max stops query

--- oracle/get_permanent_max_stops.sql
+++ oracle/get_permanent_max_stops.sql
@@ -37,24 +37,28 @@ select loc.location_id as stop_id,
 	loc.y_coordinate as y_coord
 from trans.route_def r, trans.route_stop_def rs, trans.location loc
 where loc.location_id = rs.location_id
+	and rs.route_stop_end_date > current_date
 	and rs.route_number = r.route_number
 	and rs.route_begin_date = r.route_begin_date
 	and r.route_end_date > current_date
-	--route_sub_type to in this case is MAX (as opposed to WES or other rail)
+	--route_sub_type '2' is light rail (MAX) 
 	and r.route_sub_type = 2
 	--'R' routes are 'revenue' routes, meaning that they're open to the public
 	and r.route_usage = 'R'
+	--to be a 'permanent', a stop must either be in the landmark table or 
+	--allow passengers during a service period that end's after today's date
+	and (exists (select null from landmark_location ll
+					where ll.location_id = loc.location_id
+					and exists (select null from landmark lm
+								where lm.landmark_id = ll.landmark_id
+								and exists (select null from landmark_type lt
+											where lt.landmark_id = lm.landmark_id
+											and landmark_type = 7)))
+		or loc.passenger_access_code != 'N')
 	--Some stops may or may not go into service one day are added to the system
 	--as place holders and given coordinates of 0, 0
 	and loc.x_coordinate != 0
 	and loc.y_coordinate != 0
-	and exists (select null from landmark_location ll
-				where ll.location_id = loc.location_id
-				and exists (select null from landmark lm
-							where lm.landmark_id = ll.landmark_id
-							and exists (select null from landmark_type lt
-										where lt.landmark_id = lm.landmark_id
-										and landmark_type = 7)))
 group by loc.location_id, loc.public_location_description,
 	loc.x_coordinate, loc.y_coordinate;
 
