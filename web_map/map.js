d1bf9ecdd2ed57681c5300c9036b7ca3debbcfa0
grant-humphries <grant.humphries@gmail.com>
2015-03-09 18:45:11 -0700

    continued building taxlot development web map

--- web_map/map.js
+++ web_map/map.js
@@ -3,26 +3,48 @@ var map_center = ol.proj.transform(
 	[-122.68163, 45.52129], 'EPSG:4326', 'EPSG:3857'
 );
 
+// data attributions
+var tm_attribution = new ol.Attribution({
+	html: 'tiles &copy; <a target="#" href="http://trimet.org/">TriMet</a>; map data'});
+var metro_attribution = new ol.Attribution({
+	html: 'and &copy; <a target="#" href="http://oregonmetro.gov/rlis">Oregon Metro</a>'});
+
+var attributions = [
+	tm_attribution,
+	ol.source.OSM.ATTRIBUTION,
+	metro_attribution
+];
+
+var domain = 'http://maps7.trimet.org'
+
 // map layers
-var mq_aerial = new ol.layer.Tile({
-	source: new ol.source.MapQuest({
-		layer: 'sat'
+var tm_aerial = new ol.layer.Tile({
+	source: new ol.source.XYZ({
+		attributions: attributions,
+		url: domain + '/tilecache/tilecache.py/1.0.0/hybridOSM/{z}/{x}/{y}'
 	})
 });
 
-var taxlot_layer = new ol.layer.Tile({
+var tm_carto = new ol.layer.Tile({
+	source: new ol.source.XYZ({
+		attributions: attributions,
+		url: domain + '/tilecache/tilecache.py/1.0.0/currentOSM/{z}/{x}/{y}'
+	})
+});
+
+var dev_taxlots = new ol.layer.Tile({
 	source: new ol.source.TileWMS(({
-		url: 'http://maps7.trimet.org/gis/geoserver/wms',
-		params: {'LAYERS': 'load:taxlot', 'TILED': true},
+		url: domain + '/gis/geoserver/wms',
+		params: {'LAYERS': 'misc-gis:web_map_taxlots', 'TILED': true},
 		serverType: 'geoserver'
 	}))
 });
 
-taxlot_layer.setOpacity(0.5);
+dev_taxlots.setOpacity(0.5);
 
 var layers = [
-	mq_aerial,
-	taxlot_layer
+	tm_aerial,
+	dev_taxlots
 ];
 
 // map object
@@ -31,6 +53,45 @@ var map = new ol.Map({
 	target: 'map',
 	view: new ol.View({
 		center: map_center,
-		zoom: 15
+		zoom: 15,
+		minZoom: 13,
+		maxZoom: 19
 	})
+});
+
+$(document).ready(function(){
+	$('.legend-btn').click(function(){
+		$('#legend-wrapper').toggle();
+	});
+});
+
+$(document).ready(function(){
+	$('.cntrl-wrapper').hover(function(){
+		$(this).css('background-color', 'rgba(255, 255, 255, 0.6)');
+	},
+	function(){
+		$(this).css('background-color', 'rgba(255, 255, 255, 0.4)');
+	});
+});
+
+$(document).ready(function(){
+	$('.legend-btn').hover(function(){
+		$(this).css('background-color', 'rgba(0, 60, 136, 0.7)');
+	},
+	function(){
+		$(this).css('background-color', 'rgba(0, 60, 136, 0.5)');
+	});
+});
+
+/*$(document).ready(function(){
+	$('#legend-wrapper').hide();
+});*/
+
+$(document).ready(function(){
+	$('.legend').hover(function(){
+		$(this).css('background-color', 'rgba(255, 255, 255, 0.75)');
+	},
+	function(){
+		$(this).css('background-color', 'rgba(255, 255, 255, 0.5)');
+	});
 });
