8275f4e93c5784526bb2e6723c8cf65d5689c1ea
grant-humphries <grant.humphries@gmail.com>
2015-03-20 10:28:52 -0700

    made some progress towards adding a layer control to the map

--- web_map/map.js
+++ web_map/map.js
@@ -1,9 +1,10 @@
-// map settings
+// Map settings
 var map_center = ol.proj.transform(
 	[-122.68163, 45.52129], 'EPSG:4326', 'EPSG:3857'
 );
 
-// data attributions
+
+// Data attributions
 var tm_attribution = new ol.Attribution({
 	html: 'tiles &copy; <a target="#" href="http://trimet.org/">TriMet</a>; map data'});
 var metro_attribution = new ol.Attribution({
@@ -15,9 +16,10 @@ var attributions = [
 	metro_attribution
 ];
 
+
+// Map layers
 var domain = 'http://maps7.trimet.org'
 
-// map layers
 var tm_aerial = new ol.layer.Tile({
 	source: new ol.source.XYZ({
 		attributions: attributions,
@@ -32,22 +34,26 @@ var tm_carto = new ol.layer.Tile({
 	})
 });
 
+tm_carto.setVisible(false);
+
 var dev_taxlots = new ol.layer.Tile({
-	source: new ol.source.TileWMS(({
+	source: new ol.source.TileWMS({
 		url: domain + '/gis/geoserver/wms',
 		params: {'LAYERS': 'misc-gis:web_map_taxlots', 'TILED': true},
 		serverType: 'geoserver'
-	}))
+	})
 });
 
 dev_taxlots.setOpacity(0.5);
 
 var layers = [
 	tm_aerial,
+	tm_carto,
 	dev_taxlots
 ];
 
-// map object
+
+// Map object and view
 var map = new ol.Map({
 	layers: layers,
 	target: 'map',
@@ -59,6 +65,8 @@ var map = new ol.Map({
 	})
 });
 
+
+// Legend functionality
 $(document).ready(function(){
 	$('#legend-wrapper').hide();
 });
@@ -77,6 +85,8 @@ $(document).ready(function(){
 	});
 });
 
+
+// Legend appearance
 $(document).ready(function(){
 	$('.cntrl-wrapper').hover(function(){
 		$(this).css('background-color', 'rgba(255, 255, 255, 0.6)');
@@ -96,18 +106,22 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-	$('#lp-wrapper').hide();
-});
-
-$(document).ready(function(){
 	$('.legend').hover(function(){
 		$(this).css('background-color', 'rgba(255, 255, 255, 0.75)');
 	},
 	function(){
-		$(this).css('background-color', 'rgba(255, 255, 255, 0.5)');
+		$(this).css('background-color', 'rgba(255, 255, 255, 0.6)');
 	});
 });
 
+
+// Layer panel functionality 
+/*$(document).ready(function(){
+	$('#lp-wrapper').hide();
+});*/
+
+
+// Layer panel appearance
 $(document).ready(function(){
 	$('.lyr-cntrl-btn').hover(function(){
 		$(this).css('background-color', 'rgba(255, 255, 255, 0.75)');
@@ -117,9 +131,14 @@ $(document).ready(function(){
 	});
 });
 
-/*//http://openlayers.org/en/master/examples/layer-group.js
-function bindInputs(layerid, layer) {
-  new ol.dom.Input($(layerid + ' .visible')[0])
+//http://openlayers.org/en/master/examples/layer-group.js
+/*function bindInputs(layer_id, layer) {
+  new ol.dom.Input(document.getElementById(layer_id))
       .bindTo('checked', layer, 'visible');
-  );
-}*/
+};
+*/
+var aerial_vis = new ol.dom.Input($('#aerial_vis')[0])
+aerial_vis.bindTo('checked', tm_aerial, 'visible');
+
+var carto_vis = new ol.dom.Input($('#carto_vis')[0])
+carto_vis.bindTo('checked', tm_carto, 'visible');
